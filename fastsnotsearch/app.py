84142699384b4c988e7abbf6b5ea01a03d2e15fe
Spencer Krum <nibz@cat.pdx.edu>
2013-09-21 21:27:42 -0700

    search works, pretty prints

--- fastsnotsearch/app.py
+++ fastsnotsearch/app.py
@@ -1,5 +1,11 @@
+import datetime
+
+import simplejson as json
 
 from flask import Flask
+
+from flask import render_template, abort
+
 app = Flask(__name__)
 
 import notmuch
@@ -19,9 +25,42 @@ def nibz():
 
 @app.route("/search/<term>")
 def search(term):
+    t1 = datetime.datetime.now()
     query = db.create_query(term)
     output = [ str(i) for i in list(query.search_messages()) ]
-    output.insert(0, "Found <b>{0}</b> messages matching <b>{1}</b>".format(
-      len(output), term))
+    t2 = datetime.datetime.now()
+    delta_t = (t2 - t1).microseconds / 1000.
+    output.insert(0, "Found <b>{0}</b> messages matching <b>{1}</b> in <b>{2}</b> milliseconds".format(
+      len(output), term, delta_t))
     return "\n<p>".join(output)
+
+@app.route("/v2/search/<term>")
+def searchv2(term):
+    t1 = datetime.datetime.now()
+    query = db.create_query(term)
+    msgs = []
+    for i in list(query.search_messages()):
+        msg = {}
+        msg['id'] = i.get_message_id()
+        msg['subject'] = i.get_header('subject')
+        msg['summary'] = str(i)
+        msg['xtts'] = i.get_header('X-xtts')
+        msgs.append(msg)
+
+
+    t2 = datetime.datetime.now()
+    delta_t = (t2 - t1).microseconds / 1000.
+
+
+    return render_template('search.html', msgs=msgs, num_msgs=len(msgs), term=term, delta_t=delta_t)
+
+
+@app.route("/v2/message/<msgid>")
+def messagev2(msgid):
+    msg = db.find_message(msgid).format_message_as_text()
+    ppmsg = msg.split('\n')
+    return render_template('msg.html', msg=ppmsg)
+
+
+
 
