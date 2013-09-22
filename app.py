5754856e15abff22883635cd77c07c46a38d335c
Spencer Krum <nibz@cat.pdx.edu>
2013-09-21 18:31:44 -0700

    first workingish example

--- app.py
+++ app.py
@@ -2,11 +2,30 @@
 from flask import Flask
 app = Flask(__name__)
 
+import notmuch
+
+db = notmuch.Database()
 
 @app.route("/")
 def hello():
     return "Hello World!"
 
+@app.route("/nibz")
+def nibz():
+    query = db.create_query('nibz')
+    #return " ".join(list((query.search_messages()))) # doctest:+ELLIPSIS
+    output = [ str(i) for i in list(query.search_messages()) ]
+    return "\n<p>".join(output)
+
+@app.route("/search/<term>")
+def search(term):
+    query = db.create_query(term)
+    output = [ str(i) for i in list(query.search_messages()) ]
+    output.insert(0, "Found <b>{0}</b> messages matching <b>{1}</b>".format(
+      len(output), term))
+    return "\n<p>".join(output)
+
+
 
 if __name__ == "__main__":
-    app.run('0.0.0.0')
+    app.run(host='0.0.0.0',port=4567,debug=True)
