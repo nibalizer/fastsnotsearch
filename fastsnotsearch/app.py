827b10b06bbcf7b71f8c51bf8126b34c916dd5f2
Spencer Krum <nibz@cat.pdx.edu>
2013-09-22 13:52:47 -0700

    trying to improve performance

--- fastsnotsearch/app.py
+++ fastsnotsearch/app.py
@@ -32,6 +32,39 @@ def messagev2(msgid):
     return render_template('msg.html', msg=ppmsg)
 
 
+@app.route("/search/<term>")
+def search(term):
+    t1 = datetime.datetime.now()
+    query = db.create_query(term)
+    output = [ str(i) for i in list(query.search_messages()) ]
+    t2 = datetime.datetime.now()
+    delta_t = (t2 - t1).microseconds / 1000.
+    output.insert(0, "Found <b>{0}</b> messages matching <b>{1}</b> in <b>{2}</b> milliseconds".format(
+      len(output), term, delta_t))
+    return "\n<p>".join(output)
+
+
+
+@app.route("/v2/search/<term>")
+def searchv2(term):
+    t1 = datetime.datetime.now()
+    query = db.create_query(term)
+    msgs = []
+    for i in list(query.search_messages()):
+        msg = {}
+        msg['id'] = i.get_message_id()
+        msg['subject'] = i.get_header('subject')
+        msg['summary'] = str(i)
+        msgs.append(msg)
+
+
+    t2 = datetime.datetime.now()
+    delta_t = (t2 - t1).microseconds / 1000.
+
+
+    return render_template('search.html', msgs=msgs, num_msgs=len(msgs), term=term, delta_t=delta_t)
+
+
 
 @app.route("/v3/search/snot")
 def searchv3():
@@ -42,13 +75,12 @@ def searchv3():
     for i in list(query.search_messages()):
         msg = {}
         msg['id'] = i.get_message_id()
-        msg['subject'] = i.get_header('subject').encode('utf-8')
+        msg['subject'] = i.get_header('subject')
         try:
-          msg['summary'] = str(i).encode('utf-8')
+          msg['summary'] = str(i)
         except UnicodeDecodeError:
           print i
           msg['summary'] = 'unable to get summary'
-        msg['xtts'] = i.get_header('X-xtts').encode('utf-8')
         msgs.append(msg)
 
 
